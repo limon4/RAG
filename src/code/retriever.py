33235a517fcb4422b8a825aaf666d7cff165417d
limon4 <rodriguezlimoncarlos@gmail.com>
2025-03-07 08:09:28 +0100

    Prueba de una nueva técnica de expansión

--- src/code/retriever.py
+++ src/code/retriever.py
@@ -23,7 +23,7 @@ class Retriever:
         #inicializamos el modelo para el cálculo de embeddings
         self.embeddings = HuggingFaceEmbeddings(
                 model_name=self.embedding_model,
-                model_kwargs={'device': 'cpu', 'use_auth_token': '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'},
+                model_kwargs={'device': 'cpu', 'use_auth_token': ''},
                 encode_kwargs={'normalize_embeddings': True}
         )
 
